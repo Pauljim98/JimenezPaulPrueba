7463538f8ecb3a71734fac9307028b7415c264b3
Pauljim98 <paulsoy_10@outlook.es>
2024-01-16 19:02:22 -0500

    19:00 Creacion segunda pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:pruebajimenez/router/routes.dart';
+import 'package:pruebajimenez/ui/views/login_view.dart';
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
+void main() {
+  runApp(const MyApp());
 }
 
-class _LoginScreenState extends State<LoginScreen> {
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: InputDecoration(
-                  labelText: 'Username or email',
-                ),
-              ),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  _onLoginPressed();
-                },
-                child: Text('Login'),
-              ),
-              TextButton(
-                onPressed: () {
-                  _onRegisterPressed();
-                },
-                child: Text('Register'),
-              ),
-            ],
-          ),
-        ),
-      ),
+      initialRoute: LoginView.id,
+      routes: customRoutes,
     );
-  }
-
-  void _onLoginPressed() {
-    final username = _usernameController.text;
-    final password = _passwordController.text;
-
-    // Implement logic to authenticate the user
-
-    // For example, you could check if the username and password are stored in a local database.
-
-    if (username == 'admin' && password == 'password') {
-      // Login successful
-
-      _navigateToHomeScreen();
-    } else {
-      // Login failed
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Login failed'),
-        ),
-      );
-    }
-  }
-
-  void _onRegisterPressed() {
-    // Navigate to the registration screen
-
-    Navigator.of(context).pushNamed('/register');
-  }
-
-  void _navigateToHomeScreen() {
-    // Navigate to the home screen
-
-    Navigator.of(context).pushNamed('/home');
   }
 }
---
+++ lib/ui/views/login_view.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+  static String id = 'login_view';
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // email
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.1,
+                vertical: size.height * 0.05,
+              ),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'email',
+                  labelStyle: TextStyle(
+                    color: Color(0xFFBEBCBC),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+
+            // password
+            Padding(
+              padding: EdgeInsets.only(
+                  left: size.width * .1,
+                  right: size.width * 0.1,
+                  bottom: size.height * 0.05),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(
+                    color: Color(0xFFBEBCBC),
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                onChanged: (value) {},
+              ),
+            ),
+            // button
+            ElevatedButton(
+              onPressed: () => {},
+              child: const Text(
+                'Iniciar sesion',
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
